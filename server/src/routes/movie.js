2379433245ffd131140c8e55298bf20381ebb3f0
Ivan <ivan@bitpagos.co>
2019-04-08 11:06:29 -0300

    Agrego rutas para listar y crear movies

--- server/src/routes/movie.js
+++ server/src/routes/movie.js
@@ -1,8 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const MovieModel = require('../models/movie')
+const router = express.Router()
 
 router.get('/', function (req, res) {
-    res.json([]);
-});
+	MovieModel.getAll().then((movies) =>
+		res.status(200).send(movies)
+	).catch(_ => res.status(400).send('Error al obtener las películas'))
+})
 
-module.exports = router;
+router.post('/', function (req, res) {
+	MovieModel.create(req.body).then(data =>
+		res.status(201).send(data)
+	).catch(_ => res.status(400).send('Error al crear película'))
+})
+
+module.exports = router
