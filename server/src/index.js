a8aa78c7f1fec77b2372d1d052d832685454324a
Ivan <ivan@bitpagos.co>
2019-04-08 11:05:45 -0300

    Inicializo movies

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,26 @@
-const path = require('path');
+const path = require('path')
 
-const morgan = require('morgan');
-const express = require('express');
-const app = express();
+const morgan = require('morgan')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-const movieRouter = require('./routes/movie.js');
-const client = path.resolve(__dirname, '..', '..', 'client', 'src');
+const MovieModel = require('./models/movie.js')
 
-app.use(morgan('dev'));
-app.use(express.static(client));
+const movieRouter = require('./routes/movie.js')
+
+const client = path.resolve(__dirname, '..', '..', 'client', 'src')
+
+app.use(bodyParser.json())
+
+app.use(morgan('dev'))
+app.use(express.static(client))
 
 // Rutas
-app.use('/api/v1/movie', movieRouter);
+app.use('/api/v1/movies', movieRouter)
 
 app.listen(3000, function (server) {
-    console.log('Server started');
-});
+    console.log('Server started')
+})
+
+MovieModel.initMovies()
