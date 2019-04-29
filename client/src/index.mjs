4d469f49981ed5a2f3a1527ea5faf4e723188cd4
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2019-04-29 16:20:36 -0300

    Agrega modal para agregar nuevas películas

--- client/src/index.mjs
+++ client/src/index.mjs
@@ -1,7 +1,8 @@
 import Table from './components/table.mjs'
 import movieService from './api/movie.mjs'
 
-const table = Table('#movies', {
+// Inicializamos la tabla
+window.table = Table('#movies', {
     header: [
         { label: 'Título', field: 'title' },
         { label: 'Descripción', field: 'description' },
@@ -14,12 +15,66 @@ const table = Table('#movies', {
         }
     ],
     data: [],
+    // Esta funcion se ejecuta cuando seleccionamos una pelicula
     onSelectedRow: function (row) {
         console.log(table.getSelectedRows())
     },
+    // Esta funcion se ejecuta cuando deseleccionamos una pelicula
     onDeselectedRow: function () {
         console.log(table.getSelectedRows())
     }
 })
 
+// Obtenemos todas las peliculas
 movieService.getAll().then(table.update)
+
+// Guardamos todas las referencias a elementos que vamos a
+// necesitar
+const $refs = {
+    cancelModalBtn: document.querySelector('#cancelModalBtn'),
+    saveMovieBtn: document.querySelector('#saveMovieBtn'),
+    addMovieBtn: document.querySelector('#addMovieBtn'),
+    closeModalBtn: document.querySelector('#closeModalBtn'),
+
+    modal: document.querySelector('#modal'),
+
+    movieName: document.querySelector('#movieName'),
+    moviePlot: document.querySelector('#moviePlot'),
+    movieReleaseDate: document.querySelector('#movieReleaseDate'),
+    movieCountry: document.querySelector('#movieCountry'),
+    movieWriters: document.querySelector('#movieCountry')
+}
+
+/*
+ * Abre el modal
+ */
+function openModal() {
+    $refs.modal.classList.add('is-active')
+}
+
+/*
+ * Cierra el modal
+ */
+function closeModal() {
+    $refs.modal.classList.remove('is-active')
+}
+
+/*
+ * Guarda una pelicula
+ */
+function saveMovie() {
+    const movie = {
+        name: $refs.movieName.value,
+        plot: $refs.moviePlot.value,
+        year: new Date($refs.movieReleaseDate.value),
+        country: $refs.movieCountry.value
+    }
+
+    console.log(movie)
+}
+
+// Levantamos los listeners de la app
+$refs.addMovieBtn.addEventListener('click', openModal)
+$refs.cancelModalBtn.addEventListener('click', closeModal)
+$refs.closeModalBtn.addEventListener('click', closeModal)
+$refs.saveMovieBtn.addEventListener('click', saveMovie)
