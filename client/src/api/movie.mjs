4dee28e3f11cd766ebe3c6a9de6d727218f5a2aa
Alvarez Juan Pablo <juampi.sm2@gmail.com>
2019-05-05 19:20:05 -0300

    Agregado funcionalidad eliminar

--- client/src/api/movie.mjs
+++ client/src/api/movie.mjs
@@ -3,6 +3,17 @@ function getAll() {
         .then(result => result.json())
 }
 
+function deleteMovie(id){
+	const url = '/api/v1/movies/'+id
+	return fetch(url, {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+    })
+	.then(res => res)
+    .then(json => console.log(json));
+}
+
 export default {
-    getAll
+    getAll,
+    deleteMovie
 }
