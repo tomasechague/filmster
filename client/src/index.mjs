20cc664e6087259426ffc78fb09677ade5a280c3
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2019-05-08 23:39:54 -0300

    Refactor del client para separar utils de la logica principal

--- client/src/index.mjs
+++ client/src/index.mjs
@@ -1,4 +1,5 @@
 import Table from './components/table.mjs'
+import { parseCSV } from './utils.mjs'
 import movieService from './api/movie.mjs'
 
 // Inicializamos la tabla
@@ -61,10 +62,6 @@ function openModal() {
  */
 function closeModal() {
     $refs.modal.classList.remove('is-active')
-}
-
-function parseCSV(val) {
-    return val.split(',').flatMap(v => v.split());
 }
 
 /*
